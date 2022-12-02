4734c46d2363545c576e7137b77c314e789b5bc4
workwithAarohan <aarohan.nakarmi.29@gmail.com>
2022-12-02 10:33:46 +0545

    Image Label Added

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.css']
 })
+
 export class CardsComponent implements OnInit{
   // default = "placeholder";
   // btnDisabled = true;
@@ -14,6 +15,7 @@ export class CardsComponent implements OnInit{
   imageHidden = "hidden";
 
   label = "";
+  displayLabel = "";
 
   image_src = "";
   image_name = "";
@@ -32,19 +34,20 @@ export class CardsComponent implements OnInit{
   {
     this.image.set(img_name, img_src);
     this.label = 'Image "'+  img_name + '" added successfully.';
-    console.log(this.label);
+    console.log(this.image);
   }
 
   displayImage(img_info: string) 
   {
     if(this.image.has(img_info))
     {
+      this.displayLabel = img_info + '-Image found';
       this.this_img = this.image.get(img_info);
-      
     }
     else
     {
-      this.this_img = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbCyusovfE45AJ-91ouARO_KjR0C5fpSKcLVJeW6ji&s";
+      this.displayLabel = "Image Not Found";
+      this.this_img = "https://www.freeiconspng.com/thumbs/no-image-icon/no-image-icon-15.png";
     }
 
     this.imageHidden = "";
