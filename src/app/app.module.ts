e3e2cca7e8989090b1a3535866b2c5e19643d9b8
workwithAarohan <aarohan.nakarmi.29@gmail.com>
2022-12-04 09:45:11 +0545

    Data Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardsComponent } from './components/cards/cards.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BindingComponent } from './binding/binding.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardsComponent
+    CardsComponent,
+    BindingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/binding/binding.component.ts
@@ -0,0 +1,26 @@
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-binding',
+  templateUrl: './binding.component.html',
+  styleUrls: ['./binding.component.css']
+})
+export class BindingComponent implements OnInit, AfterViewInit {
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  
+  text_label = "";
+  reset = "";
+  user_name = "";
+
+  onClick(username: string)
+  {
+    this.text_label = "Username: " + username;
+    this.reset = " ";
+    this.user_name = " ";
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
